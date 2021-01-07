e5495a6cf033ca6d7fa091f717f29aceaf3c1db9
Ke Wang <ashir.wang@gmail.com>
2021-01-07 12:22:07 -0600

    update on Dronected

---
+++ public/index.js
@@ -0,0 +1,8 @@
+function expand(){
+  var coll = document.getElementById("content");
+  if (coll.style.display === "block") {
+      coll.style.display = "none";
+    } else {
+      coll.style.display = "block";
+    }
+}
